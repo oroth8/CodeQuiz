ecc83e3244c2f597ef2ddd4403d7dce5115c9b6c
Owen Roth <oroth8@comcast.net>
2020-09-30 18:34:32 -0600

    added a question section. added a options section. added the bais for the quiz array. started to build functions to display the questions and options.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var startBtn = document.querySelector("#start");
 var display = document.querySelector("#time");
 startBtn.addEventListener("click", startTimer);
+var disQuestion = document.querySelector("#question");
+var qA = document.querySelector("#A");
+var qB = document.querySelector("#B");
+var qC = document.querySelector("#C");
+var qD = document.querySelector("#D");
+var quiz = [
+    {q: "question question question?????",
+     options:["A","B","C","D"],
+    answer: 1}
+]
 
 function startTimer() {
     var duration = 60*15;
@@ -19,3 +29,22 @@ function startTimer() {
         }
     }, 1000);
 }
+
+function startQuiz(){
+
+}
+
+function displayQuestion(){
+    disQuestion.textContent = quiz[0].q;
+
+}
+
+displayQuestion();
+
+function displayOptions(){
+    qA.textContent = quiz[0].options[0];
+    console.log(qA);
+
+}
+
+displayOptions();
