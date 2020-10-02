27c1b328d37e2e71f0e93f3228f37b552d88d986
Owen Roth <oroth8@comcast.net>
2020-10-01 18:35:30 -0600

    created quiz container with questions. next, previous buttons added. submit quiz button added. once submitted it displays initials and scores

--- script.js
+++ script.js
@@ -2,18 +2,166 @@ var startBtn = document.querySelector("#start");
 var display = document.querySelector("#time");
 startBtn.addEventListener("click", startTimer);
 var disQuestion = document.querySelector("#question");
-var qA = document.querySelector("#A");
-var qB = document.querySelector("#B");
-var qC = document.querySelector("#C");
-var qD = document.querySelector("#D");
-var quiz = [
-    {q: "question question question?????",
-     options:["A","B","C","D"],
-    answer: 1}
-]
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+  
+
+// display quiz right away
+buildQuiz();
+
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+  
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+  
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
+
+function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+  
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+  
+        // variable to store the list of possible answers
+        const answers = [];
+  
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+  
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+      }
+    );
+  
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+  
+  function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+    // keep track of user's answers
+    let numCorrect = 0;
+  
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+  
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+  
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+    displaySave();
+  }
+
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+
+startBtn.addEventListener("click",startTimer);
 
 function startTimer() {
-    var duration = 60*15;
+    var quizDisplay = document.querySelector(".quiz-container");
+    quizDisplay.setAttribute("style", "display: block;")
+    var duration = 60*3;
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -30,21 +178,23 @@ function startTimer() {
     }, 1000);
 }
 
-function startQuiz(){
+function displaySave() {
+    var SaveContainer = document.getElementById("saveGameContainer").className = "display"
 
 }
 
-function displayQuestion(){
-    disQuestion.textContent = quiz[0].q;
+var userUnt = document.querySelector("#saveInt");
+var saveBtn = document.querySelector("#saveBtn");
+var saveOutput = document.querySelector("#savedIntOutput");
 
+function displayOutput(){
+    savedIntOutput.textContent ="Your Initials are: "+userUnt.value;
+    console.log(savedIntOutput);
+
+    var scoreSave = document.querySelector("#results");
+    var scoreH4 = document.querySelector("#savedScore");
+
+    scoreH4.textContent = scoreSave.textContent;
 }
 
-displayQuestion();
-
-function displayOptions(){
-    qA.textContent = quiz[0].options[0];
-    console.log(qA);
-
-}
-
-displayOptions();
+saveBtn.addEventListener("click", displayOutput);
